decb7ecfb1bf7300e3b764965fc5f46f0e358de4
iamadityaanjana <iamadityaanjana@gmail.com>
2025-06-12 22:08:09 +0530

    Add buffer and polyfill plugins to support Node globals in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 // https://vite.dev/config/
+
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill() as any]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
+});
